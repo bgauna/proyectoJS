e1f49d5f5dd30572dbad92445d9b0f59c06ab5fb
Briant A Gauna <gauna.br@gmail.com>
2022-10-26 22:46:13 -0300

    añadido de consigna en el index, arreglo de mensajes alert

--- main.js
+++ main.js
@@ -14,37 +14,37 @@ Para esta consigna voy a dejar los CONSOLE.LOG para mostrar su uso, aunque los s
 
 const ingresarNumero = () => {
     let x;
-    x = parseInt (prompt(`Ingrese un valor positivo. Para finalizar, ingrese un valor negativo`));
+    x = parseInt(prompt(`Ingrese un valor positivo. Para finalizar, ingrese un valor negativo`));
     while (isNaN(x)) {
-        x = parseInt (prompt(`No ingrese textos, sólo números. \n Ingrese un valor positivo. Para finalizar, ingrese un valor negativo`));
+        x = parseInt(prompt(`No ingrese textos, sólo números. \n Ingrese un valor positivo. Para finalizar, ingrese un valor negativo`));
     }
     console.log(`número válido ${x}`);
     return x;
 }
 
-const resultados = (x,x2,n) => {
+const resultados = (x, x2, n) => {
     console.log(`ingreso a resultados`);
-    let promedio = x/n;
-    let varianza = (x2-n*promedio)/(n-1);
-    let desvioPromedio=(varianza/n)**(1/2);
-    let error=2*desvioPromedio;
-    let ls= promedio+error;
-    let li=promedio-error;
+    let promedio = x / n;
+    let varianza = (x2 - n * promedio) / (n - 1);
+    let desvioPromedio = (varianza / n) ** (1 / 2);
+    let error = 2 * desvioPromedio;
+    let ls = promedio + error;
+    let li = promedio - error;
     alert(`Este es el resultado de la investigación. \n El valor medio de los datos es ${promedio}. \n La varianza de los datos es ${varianza}. \n El error de la investigación es de +-${error}. \n Con una confianza del 95%, se puede decir que el parámetro poblacional se encuentra entre ${li} y ${ls}`);
 }
 
 const otroAnalisis = () => {
     let x;
     x = prompt(`¿Desea realizar otro estudio? Ingrese SI o NO`);
-    x=x.toLowerCase();
+    if (x != null) { x = x.toLowerCase(); }
     console.log(x); //// TIENE PROBLEMA CUANDO EL USUARIO COLOCA CANCELAR
-    while (!(x==='si'||x==='no')) {
+    while (!(x === 'si' || x === 'no')) {
         x = prompt(`No ingresó una respuesta válida. \n ¿Desea realizar otro estudio? Ingrese SI o NO`);
-        x=x.toLowerCase();
+        if (x != null) { x = x.toLowerCase(); }
         console.log(x);
     }
     console.log(`respuesta válida ${x}`);
-    if (x==='si'){
+    if (x === 'si') {
         return true;
     } else {
         return false;
@@ -52,32 +52,32 @@ const otroAnalisis = () => {
 }
 
 
-let numero, suma=0, sumaCuadrado=0, n=0, neg=false, otro=true;
+let numero, suma = 0, sumaCuadrado = 0, n = 0, neg = false, otro = true;
 
-alert(`Este programa es un asistente de muestreo. \n Eres un investigador que salió a hacer una encuesta. La única pregunta de tu encuesta es "¿Cuántas horas semanales haces ejercicio?". A continuación, debes ingresar los valores. \n BG`)
-do{
-    suma=0; 
-    sumaCuadrado=0; 
-    n=0; 
-    neg=false;
-    otro=false;
-    do{
-        numero=ingresarNumero();
-        if (numero>=0){
-            suma=suma+numero;
-            sumaCuadrado=sumaCuadrado+numero**2;
+alert(`Este programa es un asistente de muestreo. \n Eres un investigador que salió a hacer una encuesta. La única pregunta de tu encuesta es "¿Cuántas horas semanales haces ejercicio?". A continuación, debes ingresar los valores. \n BG`);
+do {
+    suma = 0;
+    sumaCuadrado = 0;
+    n = 0;
+    neg = false;
+    otro = false;
+    do {
+        numero = ingresarNumero();
+        if (numero >= 0) {
+            suma = suma + numero;
+            sumaCuadrado = sumaCuadrado + numero ** 2;
             n++;
         } else {
-            neg=true;
+            neg = true;
         }
-    } while (neg===false);
+    } while (neg === false);
 
-    console.log(`Resultados ${suma}; ${sumaCuadrado}; ${n}`)
-    if(n>=2){
-    resultados(suma,sumaCuadrado,n);
+    console.log(`Resultados ${suma}; ${sumaCuadrado}; ${n}`);
+    if (n >= 2) {
+        resultados(suma, sumaCuadrado, n);
     } else {
-        alert (`Lamentablemente, no es posible hacer el análisis con tan pocos elementos ingresados.`)
+        alert(`Ingresó menos de 3 valores válidos. \nLamentablemente, no es posible hacer el análisis con tan pocos elementos ingresados.`);
     }
-    otro=otroAnalisis();
+    otro = otroAnalisis();
 } while (otro);
-alert(`¡Gracias por utilizar nuestra calculadora de muestreo! \n BG`)
+alert(`¡Gracias por utilizar nuestra calculadora de muestreo! \n BG`);
